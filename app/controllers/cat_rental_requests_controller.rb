086b881909f0ccfa6c1039466b759d3df9629606
Esther S Weon <esther.s.weon@gmail.com>
2014-06-01 09:05:11 -0700

    Revised cat rental request new form

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -1,19 +1,18 @@
 class CatRentalRequestsController < ApplicationController
 
+  def index
+    @cat_rental_requests = CatRentalRequest.all
+    render :index
+  end
 
   def create
     @cat_rental_request = CatRentalRequest.new(cat_rental_request_params)
     if @cat_rental_request.save
-      redirect_to cat_url @cat_rental_request.cat
+      redirect_to cat_url(@cat_rental_request.cat)
     else
       flash[:invalid_request] = "You can't rent that cat on those days."
       redirect_to new_cat_rental_request_url
     end
-  end
-
-  def index
-    @cat_rental_requests = CatRentalRequest.all
-    render :index
   end
 
   def approve
